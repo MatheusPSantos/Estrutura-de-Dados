deec7632dc06aa450fe49748ebf59f01f53913ab
MatheusPSantos <matheuspereira8187@gmail.com>
2017-09-12 22:05:15 -0300

    first commit

---
+++ listas/listaDuplamenteEncadeada.c
@@ -0,0 +1,28 @@
+// Funções para lista duplamente encadeada
+
+// estrutura da lista
+struct lista2 {
+    struct lista2 * ant;
+    struct lista2 * prox;
+    int info;
+};
+
+typedef struct lista2 Lista2;
+
+// inicialização da lista com NULL
+Lista2 * iniciar(void) {
+    return NULL;
+}
+
+// inserção na lista dupla
+Lista2 * inserir(Lista2 * L, int n) {
+    Lista2 * novo;
+    novo = (Lista2*)malloc(sizeof(Lista2));
+    novo->info = n;
+    novo->ant = NULL;
+    novo->prox = L;
+    if(L != NULL)
+        L->ant = novo;
+
+    return novo;
+}
