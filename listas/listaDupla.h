9410ecfdc0feae392f65e266b79cecacae35a1ae
MatheusPSantos <matheuspereira8187@gmail.com>
2017-09-13 14:47:23 -0300

    biblioteca com as funções básicas

---
+++ listas/listaDupla.h
@@ -0,0 +1,20 @@
+// Biblioteca das funções de Lista Duplamente Encadeada
+#include "listaDupla.c"
+
+// estrutura
+typedef struct lista2 Lista2;
+
+// função de inicialização da lista dupla
+Lista2 * iniciar(void);
+
+// inserção na lista dupla
+Lista2 * inserir(Lista2 * L, int n);
+
+// inserção no final
+Lista2 * inserirFim(Lista2 * L, int n);
+
+// inserção ordenada
+Lista2 * inserirOrdem(Lista2 * L, int n);
+
+// impressão da lista
+void imprimir(Lista2 * L);
