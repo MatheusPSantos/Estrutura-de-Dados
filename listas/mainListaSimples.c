bce81b75be667e1a39cd5a34a1b22bbbf7cdeb5c
MatheusPSantos <matheuspereira8187@gmail.com>
2017-09-23 15:59:42 -0300

    código para execução e teste das funções

---
+++ listas/mainListaSimples.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listaSimples.h"
+
+int main() {
+
+	/*	Exemplo de uso
+		A saida deste programa deverá ser:
+		Lista -->
+		4 -> 2 -> 1 -> 3 -> 5 -> NULL
+	*
+	*
+	
+	Lista * l;
+	l = iniciar();
+
+	l = inserir(l, 1);
+	l = inserir(l, 2);
+	l = inserirFinal(l, 3);
+	l = inserir(l, 4);
+	l = inserirFinal(l, 5);
+
+	printf("Lista -->\n");
+	imprimir(l);
+
+	*/
+
+	/*
+		A saída deverá ser:
+		
+		Lista -->
+		6 -> 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+		removendo o 4
+		Lista atualizada -->
+		6 -> 5 -> 4 -> 2 -> 1 -> NULL
+
+
+	Lista * l;
+	l = iniciar();
+
+	l = inserir(l, 1);
+	l = inserir(l, 2);
+	l = inserir(l, 3);
+	l = inserir(l, 4);
+	l = inserir(l, 5);
+	l = inserir(l, 6);
+
+	printf("Lista -->\n");
+	imprimir(l);
+	printf("removendo o 3 ...\n");
+	l = remover(l, 3);
+	printf("Lista atualizada -->\n");
+	imprimir(l);
+
+	*/
+
+	return 0;
+
+
+
+}
