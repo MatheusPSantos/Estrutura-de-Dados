926b9a17e0db65d1bc202a094d04c24c3dfcc190
MatheusPSantos <matheuspereira8187@gmail.com>
2017-09-23 15:58:42 -0300

    cabeçalhos das funções de lista simples

---
+++ listas/listaSimples.h
@@ -0,0 +1,20 @@
+// Biblioteca com as funções de Lista Simples
+#include "listaSimples.c"
+
+// definição da lista 
+typedef struct lista Lista;
+
+// inicialização da lista
+Lista * iniciar(void);
+
+// inserção na lista
+Lista * inserir(Lista * L, int n);
+
+// inserção no final da lista
+Lista * inserirFinal(Lista * L, int n);
+
+// remoção de um elemento da lista
+Lista * remover(Lista * L, int n);
+
+// função para impressão da lista
+void imprimir(Lista * L);
