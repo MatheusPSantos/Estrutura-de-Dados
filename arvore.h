1c7f226c37040852a04bb4ee93702b2eec8a1ec9
MatheusPSantos <matheuspereira8187@gmail.com>
2017-11-07 23:03:41 -0200

    estrutura e funções de árvores

--- arvore.h
+++
@@ -1,7 +0,0 @@
-#include "arvore.c"
-typedef struct arv Arv;
-Arv* arv_criavazia();
-Arv* arv_cria(int v, Arv* sae, Arv* sad);
-Arv* arv_libera(Arv* a);
-int arv_pertence(Arv* a, int v);
-void arv_imprime(Arv* a);
