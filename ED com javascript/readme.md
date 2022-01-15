9770a5f8aed87b6547f50c9797d38ec8f2923258
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2022-01-14 23:16:06 -0300

    feat: adicionando a classe de fila

---
+++ ED com javascript/readme.md
@@ -0,0 +1,6 @@
+
+
+## Filas
+
+Uma fila é uma coleção de items ordenados baesado em FIFO (First In First Out). A adição do novo elemento é feita na cauda, tail, e a remoção é feita na frente, head.
+
---
+++ ED com javascript/src/data-structures/queue/queue.js
@@ -0,0 +1,51 @@
+class Queue {
+    constructor() {
+        this.count = 0;
+        this.lowestCount = 0;
+        this.items = {};
+    }
+
+    // inserção de novo elemento na fila
+    enqueue(element) {
+        this.items[this.count] = element;
+        this.count++;
+    }
+
+    // remoção do elemento da fila
+    dequeue() {
+        if (this.isEmpty()) return undefined;
+        const result = this.items[this.lowestCount];
+        delete this.items[this.lowestCount];
+        this.lowestCount++;
+        return result;
+    }
+
+    // retorna o elemento da frente da fila
+    peek() {
+        if (this.isEmpty()) return undefined;
+        return this.items[this.lowestCount];
+    }
+
+    isEmpty() {
+        return this.size() === 0;
+    }
+
+    size() {
+        return this.count - this.lowestCount;
+    }
+
+    clear() {
+        this.items = {};
+        this.count = 0;
+        this.lowestCount = 0;
+    }
+
+    toString() {
+        if (this.isEmpty()) return '';
+        let objString = `${this.items[this.lowestCount]}`;
+        for (let index = this.lowestCount + 1; index < this.count; index++) {
+            objString = `${objString},${this.items[index]}`;
+        }
+        return objString;
+    }
+}
