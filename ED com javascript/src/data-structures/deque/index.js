ec85e96d1f98ae7e91c6f10b2b2f156269aaa81f
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2022-01-15 11:30:26 -0300

    hot potato e palindromo exemplos

--- ED com javascript/src/data-structures/deque/index.js
+++ ED com javascript/src/data-structures/deque/index.js
@@ -1,4 +1,5 @@
 const Deque = require("./deque");
+const palindromeChecker = require("./palindromo");
 
 const deque = new Deque();
 
@@ -16,3 +17,11 @@ deque.removeBack();
 console.log(deque.toString());
 deque.addFront('John');
 console.log(deque.toString());
+
+console.log("======= PALINDROME CHECKER ================");
+console.log('a', palindromeChecker('a'));
+console.log('aa', palindromeChecker('aa'));
+console.log('kayak', palindromeChecker('kayak'));
+console.log('level', palindromeChecker('level'));
+console.log('Was it a car or a cat i saw', palindromeChecker('Was it a car or a cat i saw'));
+console.log('Step on no pets', palindromeChecker('Step on no pets'));
--- ED com javascript/src/data-structures/queue/index.js
+++ ED com javascript/src/data-structures/queue/index.js
@@ -14,3 +14,13 @@ console.log(queue.isEmpty());
 queue.dequeue();
 queue.dequeue();
 console.log(queue.toString());
+
+console.log("=========== HOT POTATO GAME ===========");
+const { hotPotato } = require('./hotPotato');
+
+const names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl'];
+const result = hotPotato(names, 7);
+result.eliminated.forEach(name => {
+    console.log(`${name} was eliminated from hot potato game.`);
+});
+console.log(`The winner is: ${result.winner}`);
